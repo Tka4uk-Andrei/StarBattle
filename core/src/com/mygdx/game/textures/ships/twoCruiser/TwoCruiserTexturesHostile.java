ea9352de3472707a25c1d799479139b192f082a3
Andrey Tkachuk <tka4uk.andrei@yandex.ru>
2016-05-25 19:29:55 +0200

    Add new ship textures

---
+++ core/src/com/mygdx/game/textures/ships/twoCruiser/TwoCruiserTexturesHostile.java
@@ -0,0 +1,43 @@
+package com.mygdx.game.textures.ships.twoCruiser;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.Array;
+import com.mygdx.game.system.TexturesPack;
+import com.mygdx.game.textures.TexturesGetter;
+
+public class TwoCruiserTexturesHostile extends TexturesPack {
+
+    private Array<Texture> textures = TexturesGetter.getTextures(Constants.DIR, Constants.FILES);
+
+    @Override
+    public Array<Texture> getTextures() {
+        return textures;
+    }
+
+    @Override
+    public int getFrameTime() {
+        return Constants.FRAME_TIME;
+    }
+
+    @Override
+    public float getDeltaDegree() {
+        return Constants.DELTA_DEGREE;
+    }
+
+    @Override
+    public void dispose() {
+        for (Texture texture : textures)
+            texture.dispose();
+    }
+
+    private static class Constants {
+
+        public static final String DIR = "TwoCruiser/hostile/";
+
+        public static final String FILES[] = new String[]{"1.png"};
+
+        public static final int FRAME_TIME = 60;
+
+        public static float DELTA_DEGREE = 3;
+    }
+}

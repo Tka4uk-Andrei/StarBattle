92450f7fa210c94f22d609b3ef8d05084116812b
Andrey Tkachuk <tka4uk.andrei@yandex.ru>
2016-06-05 21:00:47 +0200

    Levels patch

--- core/src/com/mygdx/game/system/Levels.java
+++ core/src/com/mygdx/game/system/Levels.java
@@ -20,15 +20,15 @@ public class Levels {
         float py = (screenHeight - 128) / (mapHeight * 2);
 
         stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 1,
-                screenHeight - (128 / 2 + py + py * 2 * 0)), 0, StarModel.Constants.Types.FACTORY,
+                screenHeight - (128 / 2 + py + py * 2 * 0)), 0, StarModel.Constants.Types.MINE,
                 new int[]{1, 2, 3}, new FleetModel()));
 
         stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 4,
-                screenHeight - (128 / 2 + py + py * 2 * 0)), 0, StarModel.Constants.Types.FACTORY,
+                screenHeight - (128 / 2 + py + py * 2 * 0)), 0, StarModel.Constants.Types.ADVANCED_FACTORY,
                 new int[]{0, 4, 5}, new FleetModel()));
 
         stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 0,
-                screenHeight - (128 / 2 + py + py * 2 * 1)), 0, StarModel.Constants.Types.ADVANCED_FACTORY,
+                screenHeight - (128 / 2 + py + py * 2 * 1)), 0, StarModel.Constants.Types.SMALL,
                 new int[]{0, 3, 6, 8}, new FleetModel()));
 
         stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 2,
@@ -36,27 +36,27 @@ public class Levels {
                 new int[]{0, 2, 4, 9}, new FleetModel()));
 
         stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 3,
-                screenHeight - (128 / 2 + py + py * 2 * 1)), 0, StarModel.Constants.Types.ADVANCED_FACTORY,
+                screenHeight - (128 / 2 + py + py * 2 * 1)), 0, StarModel.Constants.Types.SMALL,
                 new int[]{1, 3, 5, 9, 10}, new FleetModel()));
 
         stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 5,
-                screenHeight - (128 / 2 + py + py * 2 * 1)), 0, StarModel.Constants.Types.MINE,
+                screenHeight - (128 / 2 + py + py * 2 * 1)), 0, StarModel.Constants.Types.SMALL,
                 new int[]{1, 4, 7, 11}, new FleetModel()));
 
         stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 1,
-                screenHeight - (128 / 2 + py + py * 2 * 2)), 0, StarModel.Constants.Types.SMALL,
+                screenHeight - (128 / 2 + py + py * 2 * 2)), 0, StarModel.Constants.Types.FACTORY,
                 new int[]{2}, new FleetModel()));
 
         stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 4,
-                screenHeight - (128 / 2 + py + py * 2 * 2)), 0, StarModel.Constants.Types.SMALL,
+                screenHeight - (128 / 2 + py + py * 2 * 2)), 0, StarModel.Constants.Types.FACTORY,
                 new int[]{5}, new FleetModel()));
 
         stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 0,
-                screenHeight - (128 / 2 + py + py * 2 * 3)), 0, StarModel.Constants.Types.FACTORY,
+                screenHeight - (128 / 2 + py + py * 2 * 3)), 0, StarModel.Constants.Types.MINE,
                 new int[]{2, 9}, new FleetModel()));
 
         stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 2,
-                screenHeight - (128 / 2 + py + py * 2 * 3)), 0, StarModel.Constants.Types.MINE,
+                screenHeight - (128 / 2 + py + py * 2 * 3)), 0, StarModel.Constants.Types.SMALL,
                 new int[]{3, 8, 4, 10}, new FleetModel()));
 
 
@@ -65,74 +65,7 @@ public class Levels {
                 new int[]{4, 9, 11}, new FleetModel()));
 
         stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 5,
-                screenHeight - (128 / 2 + py + py * 2 * 3)), 0, StarModel.Constants.Types.FACTORY,
-                new int[]{5, 10}, new FleetModel()));
-
-        stars.get(6).setMastership(Constants.Sides.FRIENDLY);
-        stars.get(7).setMastership(Constants.Sides.HOSTILE);
-
-        return stars;
-    }
-
-    public static Array<StarModel> getStars2() {
-        Array<StarModel> stars = new Array<StarModel>();
-
-        int screenHeight = Gdx.graphics.getHeight();
-        int screenWidth = Gdx.graphics.getWidth();
-
-        int mapWidth = 6;
-        int mapHeight = 4;
-
-        float px = (screenWidth - 128 - 128) / (mapWidth * 2);
-        float py = (screenHeight - 128) / (mapHeight * 2);
-
-        stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 1,
-                screenHeight - (128 / 2 + py + py * 2 * 0)), 0, StarModel.Constants.Types.FACTORY,
-                new int[]{1, 2, 3}, new FleetModel()));
-
-        stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 4,
-                screenHeight - (128 / 2 + py + py * 2 * 0)), 0, StarModel.Constants.Types.FACTORY,
-                new int[]{0, 4, 5}, new FleetModel()));
-
-        stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 0,
-                screenHeight - (128 / 2 + py + py * 2 * 1)), 0, StarModel.Constants.Types.ADVANCED_FACTORY,
-                new int[]{0, 3, 6, 8}, new FleetModel()));
-
-        stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 2,
-                screenHeight - (128 / 2 + py + py * 2 * 1)), 0, StarModel.Constants.Types.SMALL,
-                new int[]{0, 2, 4, 9}, new FleetModel()));
-
-        stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 3,
-                screenHeight - (128 / 2 + py + py * 2 * 1)), 0, StarModel.Constants.Types.ADVANCED_FACTORY,
-                new int[]{1, 3, 5, 9, 10}, new FleetModel()));
-
-        stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 5,
-                screenHeight - (128 / 2 + py + py * 2 * 1)), 0, StarModel.Constants.Types.MINE,
-                new int[]{1, 4, 7, 11}, new FleetModel()));
-
-        stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 1,
-                screenHeight - (128 / 2 + py + py * 2 * 2)), 0, StarModel.Constants.Types.SMALL,
-                new int[]{2}, new FleetModel()));
-
-        stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 4,
-                screenHeight - (128 / 2 + py + py * 2 * 2)), 0, StarModel.Constants.Types.SMALL,
-                new int[]{5}, new FleetModel()));
-
-        stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 0,
-                screenHeight - (128 / 2 + py + py * 2 * 3)), 0, StarModel.Constants.Types.FACTORY,
-                new int[]{2, 9}, new FleetModel()));
-
-        stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 2,
-                screenHeight - (128 / 2 + py + py * 2 * 3)), 0, StarModel.Constants.Types.MINE,
-                new int[]{3, 8, 4, 10}, new FleetModel()));
-
-
-        stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 3,
-                screenHeight - (128 / 2 + py + py * 2 * 3)), 0, StarModel.Constants.Types.SMALL,
-                new int[]{4, 9, 11}, new FleetModel()));
-
-        stars.add(new StarModel(new Point(128 / 2 + px + px * 2 * 5,
-                screenHeight - (128 / 2 + py + py * 2 * 3)), 0, StarModel.Constants.Types.FACTORY,
+                screenHeight - (128 / 2 + py + py * 2 * 3)), 0, StarModel.Constants.Types.ADVANCED_FACTORY,
                 new int[]{5, 10}, new FleetModel()));
 
         stars.get(6).setMastership(Constants.Sides.FRIENDLY);
